83b99eccd1ca03f9ea84d2e2b7c462fc3d660fcd
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-23 18:10:53 +0200

    feat(frontend): add fetchData

---
+++ frontend/src/api/fetchData.ts
@@ -0,0 +1,63 @@
+interface fetchInterface {
+  <T>(
+    apiUrl: string,
+    method: string,
+    credentials: RequestCredentials,
+    myData: any,
+    options?: RequestInit,
+  ): Promise<T>;
+}
+
+export const fetchData: fetchInterface = async (
+  apiUrl,
+  method,
+  credentials,
+  myData,
+  options = {},
+) => {
+  const dataBody = myData !== undefined ? { ...myData } : {};
+  const isFormData = myData instanceof FormData;
+  // Diciamo a TypeScript che body può essere testo, null O un oggetto FormData
+  let body: string | null | FormData =
+    method !== "GET" ? JSON.stringify(dataBody) : null;
+
+  if (isFormData) {
+    body = myData;
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      method,
+      headers: isFormData
+        ? undefined
+        : {
+            "Content-Type": "application/json",
+          },
+      credentials,
+      body,
+      ...options,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => null);
+      return {
+        error: true,
+        errorMessage:
+          errorData?.message || `Errore HTTP! Status: ${response.status}`,
+        status: response.status,
+      };
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (err: any) {
+    return err.name === "AbortError"
+      ? { error: true, aborted: true }
+      : {
+          error: true,
+          errorMessage: err.message,
+          status: err.response?.status || 500,
+        };
+  }
+};
