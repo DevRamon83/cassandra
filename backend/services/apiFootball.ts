b62ce865cdfaee84f958bfde1d69957cc85aca92
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-24 18:07:47 +0200

    feat(backend): add apiFootball service

---
+++ backend/services/apiFootball.ts
@@ -0,0 +1,42 @@
+import type { Interfaces } from "../../shared/index.ts";
+
+const defineLeague = (league: string) => {
+  switch (league) {
+    case "serieA":
+      return "it.1";
+
+    default:
+      return null;
+  }
+};
+
+export async function apiFootball(data: Interfaces.GameWeek) {
+  const { league, season } = data;
+  const mySeason = season.substring(0, 4) + "-" + season.substring(7, 9);
+
+  const myLeague = defineLeague(league);
+  const baseUrl =
+    "https://raw.githubusercontent.com/openfootball/football.json/master";
+  const endpoint = `${mySeason}/${myLeague}.json`;
+  const url = `${baseUrl}/${endpoint}`;
+  console.log(url);
+  try {
+    const response = await fetch(`${baseUrl}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      return { error: true, errorMsg: "noResponse" };
+    }
+
+    const data = await response.json();
+
+    return { error: false, data };
+  } catch (err) {
+    const error = err as Error;
+    return { error: true, errorMsg: error.message };
+  }
+}
