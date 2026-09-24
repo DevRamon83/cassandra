5f0a26a7904ff96c2f45a599449a6663755b18a9
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-24 18:11:21 +0200

    feat(frontend): add GameWeek component to Faq page for test

--- frontend/src/pages/Faq.tsx
+++ frontend/src/pages/Faq.tsx
@@ -1,22 +1,9 @@
-import { useEffect } from "react";
+import GameWeek from "../components/GameWeek";
 
 export default function Faq() {
-  useEffect(() => {
-    const test = async () => {
-      try {
-        const response = await fetch("URL_DELLA_TUA_FUNZIONE?tipo=lastMatch");
-        const data = await response.text();
-        console.log(data);
-      } catch (error) {
-        console.error("Errore:", error);
-      }
-    };
-
-    test();
-  }, []);
   return (
     <>
-      <p>faq</p>
+      <GameWeek />
     </>
   );
 }
