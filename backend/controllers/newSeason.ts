99221fb5b4fd093303e992ce2aff9366609077dc
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-26 17:24:49 +0200

    draft(backend): add pupulateTeams to newSeason controller

--- backend/controllers/newSeason.ts
+++ backend/controllers/newSeason.ts
@@ -1,20 +1,34 @@
 import { Context } from "@hono/hono";
 import { getBody } from "../helpers/getBody.ts";
 import { apiFootball } from "../services/apiFootball.ts";
-import { getTeams } from "../helpers/getTeams.ts";
+import { populateTeams } from "../pipelines/populateTeams.ts";
+import { errors, messages } from "../../shared/index.ts";
 
 export const newSeason = async (c: Context) => {
   try {
     const body = await getBody(c);
     if (!body) {
-      // handle return
+      return c.json({ error: true, errorMsg: errors.bodyMissing }, 500);
     }
 
     const seasonData = await apiFootball(body);
-    const teams = getTeams(seasonData.data);
-    console.log(teams);
 
-    return c.json({ error: false, data: seasonData }, 200);
+    if (!seasonData || seasonData.error || !seasonData.data) {
+      return c.json(
+        {
+          error: true,
+          errorMsg: seasonData.errorMsg || errors.apiFootball,
+        },
+        500,
+      );
+    }
+
+    const response = await populateTeams(seasonData.data);
+
+    if (!response)
+      return c.json({ error: true, errorMsg: errors.teamsPopulation }, 500);
+
+    return c.json({ error: false, data: messages.seasonSeeding }, 200);
   } catch (err) {
     const error = err as Error;
 
